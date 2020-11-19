145b831a27a98703826fdab91dd0baa89fd4aa5b
yj <yj.leee@gmail.com>
2020-11-19 23:32:03 +0900

    MODIFY: 로그인, 회원가입 뷰 수정
    - 예외처리 분리
    - 사용하지 않는 모듈 삭제
    - KeyError에 password 검사 추가

--- users/views.py
+++ users/views.py
@@ -3,26 +3,27 @@ import re
 import jwt
 import bcrypt
 
-from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 from django.conf import settings
+
 from .utils import login_decorator
 from .models import User
 
 class UserView(View):
     def post(self, request):
-        data = json.loads(request.body)
         try:
+            data = json.loads(request.body)
             email_check = re.compile('^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$')
+
             if not re.match(email_check, data['email']):
-                return JsonResponse({'message' : 'EMAIL_ERROR'}, status=400)
+                return JsonResponse({'message' : 'EMAIL_INVALID'}, status=400)
 
             if len(data['password']) < 6:
-                return JsonREsponse({'message' : 'PASSWORD_ERROR'}, status=400)
+                return JsonResponse({'message' : 'PASSWORD_INVALID'}, status=400)
 
             if User.objects.filter(email=data['email']).exists():
-                return JsonResponse({'message' : 'EMAIL_ERROR'}, status=400)
+                return JsonResponse({'message' : 'DUPLIRCATION_EMAIL'}, status=400)
 
             password         = data['password'].encode('utf-8')
             hashed_password  = bcrypt.hashpw(password, bcrypt.gensalt())
@@ -34,14 +35,18 @@ class UserView(View):
                 password  = decode_password)
             return JsonResponse({'message' : 'SUCCESS'}, status=200)
 
-        except Exception:
-            return JsonResponse({'message' : 'ERROR'}, status=400)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+        except (AttributeError, TypeError):
+            return JsonResponse({'message' : 'PASSWORD_MUST_BE_STRING_TYPE'}, status=400)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'message' : 'REQUEST_IS_NONE'}, status=400)
 
 class LoginView(View):
     def post(self, request):
-        data = json.loads(request.body)
         try:
-            if 'email' not in data:
+            data = json.loads(request.body)
+            if 'email' not in data or 'password' not in data:
                 return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
 
             if User.objects.filter(email=data['email']).exists():
@@ -52,7 +57,9 @@ class LoginView(View):
                 access_token  = jwt.encode({'id':user.id},SECRET, algorithm='HS256')
                 access_token  = access_token.decode('utf-8')
                 return JsonResponse({'token':access_token}, status=200)
-            return JsonResponse({'message' : 'Login Failed - Wrong Password'}, status=400)
+            return JsonResponse({'message' : 'WRONG_PASSWORD'}, status=400)
 
-        except Exception:
-            return JsonResponse({'message' : 'ERROR'}, status=400)
+        except (AttributeError, TypeError):
+            return JsonResponse({'message' : 'ALL_KEY_TYPE_MUST_BE_STRING'}, status=400)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({'message' : 'REQUEST_IS_NONE'}, status=400)
