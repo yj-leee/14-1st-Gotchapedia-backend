6a59f6f09955e67c2fc674cd275681e54109bd0a
yj <yj.leee@gmail.com>
2020-11-19 17:02:16 +0900

    [add] users/views.py 작성

---
+++ users/utils.py
@@ -0,0 +1,25 @@
+import jwt
+import json
+import requests
+
+from django.http import JsonResponse
+from django.conf import settings
+from users.models import User
+
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token  = request.headers['Authorization']
+            payload       = jwt.decode(access_token, settings.SECRET_KEY, algorithms='HS256')
+            user          = User.objects.get(id=payload['id'])
+            request.user  = user.id
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message':'INVALID_TOKEN'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message':'INVALID_TOKEN'}, status=400)
+        return func(self, request, *args, **kwargs)
+
+    return wrapper
--- users/views.py
+++ users/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+import json
+import re
+import jwt
+import bcrypt
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from django.conf import settings
+from .utils import login_decorator
+from .models import User
+
+class UserView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            email_check = re.compile('^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$')
+            if not re.match(email_check, data['email']):
+                return JsonResponse({'message' : 'EMAIL_ERROR'}, status=400)
+
+            if len(data['password']) < 6:
+                return JsonREsponse({'message' : 'PASSWORD_ERROR'}, status=400)
+
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message' : 'EMAIL_ERROR'}, status=400)
+
+            password         = data['password'].encode('utf-8')
+            hashed_password  = bcrypt.hashpw(password, bcrypt.gensalt())
+            decode_password  = hashed_password.decode('utf-8')
+
+            User.objects.create(
+                name      = data['name'],
+                email     = data['email'],
+                password  = decode_password)
+            return JsonResponse({'message' : 'SUCCESS'}, status=200)
+
+        except Exception:
+            return JsonResponse({'message' : 'ERROR'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if 'email' not in data:
+                return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+
+            if User.objects.filter(email=data['email']).exists():
+                user = User.objects.get(email=data['email'])
+
+            if bcrypt.checkpw(data['password'].encode('utf-8'),user.password.encode('utf-8')):
+                SECRET        = settings.SECRET_KEY
+                access_token  = jwt.encode({'id':user.id},SECRET, algorithm='HS256')
+                access_token  = access_token.decode('utf-8')
+                return JsonResponse({'token':access_token}, status=200)
+            return JsonResponse({'message' : 'Login Failed - Wrong Password'}, status=400)
+
+        except Exception:
+            return JsonResponse({'message' : 'ERROR'}, status=400)
