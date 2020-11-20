2c99c00ecf9e6084c7ca2fad2c2e717380f7ad70
GyuSeok <gyskjng@gmail.com>
2020-11-20 16:18:30 +0900

    MODIFY: 메인 페이지 뷰 수정
    - 쿼리 스트링 사용
    - 전달인자
    	- id: 원하는 사용자 아이디
    	- range: 범위
    - 쿼리 셋 더 단순한 구조로 변경

--- movie/views.py
+++ movie/views.py
@@ -9,25 +9,20 @@ from analysis.models import Star
 class UserFavoriteView(View):
     def get(self, request):
         account_id = request.GET.get('id')
-        list_range = request.GET.get('range')
+        list_range = request.GET.get('range', 10)
 
-        if not account_id or not list_range:
+        if not account_id:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
         try:
-            stars  = Star.objects.filter(user_id=int(account_id)).order_by('-point')[:int(list_range)]
-            movies = [star.movie for star in stars]
-
-            content = {
-                'message': 'SUCCESS',
+            rank_data = {
                 'data': [{
-                            'imageURL': movie.main_image,
-                            'title': movie.name,
-                            'rate': f'{stars[i].point}',
-                            'rank': i+1,
-                            'date': f'{movie.opening_at.year} . {movie.country}'
-                        } for i, movie in enumerate(movies)]
-            }
-            return JsonResponse(content, status=200)
+                            'imageURL': star.movie.main_image,
+                            'title': star.movie.name,
+                            'rate': star.point,
+                            'date': f'{star.movie.opening_at.year} . {star.movie.country}'
+                 } for star in Star.objects.filter(user_id=int(account_id)).order_by('-point')[:int(list_range)]]
+            }#select related 써서 데이터 베이스 호출 줄일 것!!!!! 
+            return JsonResponse(rank_data, status=200)
         except ValueError:
             return JsonResponse({'message': 'INSTANCE_IS_NOT_NUMBER'}, status=400)
