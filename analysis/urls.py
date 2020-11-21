3040b0a6e0fc6a36ddc5542a8f54552474bb4b0b
GyuSeok <gyskjng@gmail.com>
2020-11-21 14:01:55 +0900

    ADD: 장르별/국가별 선호순위 기능 추가
    - 유저가 별점을 준 영화들의 장르별/국가별 순위, 평균 점수(별점 백분위), 평가 개수 출력
    - 장르별/국가별 쿼리 스트링으로 받아서 하나의 뷰에서 처리할 수 있도록 작성

---
+++ analysis/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import FavoriteView
+
+urlpatterns = [
+    path("/favorite", FavoriteView.as_view()),
+]
+
--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import JsonResponse
+
+from movie.models import Movie
+from analysis.models import Star
+from users.utils import login_decorator
+
+class FavoriteView(View):
+    @login_decorator
+    def get(self, request):
+        account_id = request.user
+        category   = request.GET.get('category', 'genre') # '장르(genre)' 또는 '국가(country)'
+        results = {}
+
+        try:
+            if category == 'genre':
+                stars = Star.objects.select_related('movie').prefetch_related('movie__genre_set').filter(user_id=account_id)
+                for star in stars:
+                    if not star.movie.genre_set.name in results:
+                        results[star.movie.genre_set.name] = {'count': 0, 'score': 0}
+                    results[star.movie.genre_set.name]['count'] += 1
+                    results[star.movie.genre_set.name]['score'] += star.point
+
+            if category == 'country':
+                stars = Star.objects.select_related('movie').filter(user_id=account_id)
+                for star in stars:
+                    if not star.movie.country in results:
+                        results[star.movie.country] = {'count': 0, 'score': 0}
+                    results[star.movie.country]['count'] += 1
+                    results[star.movie.country]['score'] += star.point
+
+            for result in results.values():
+                result['score'] = int(result['score'] / result['count'] * 20)
+
+            content = {
+                'data': sorted([{
+                            'label': key,
+                            'score': value['score'],
+                            'count': value['count'],
+                } for key, value in results.items()], key=lambda data: data['score'], reverse=True)
+            }
+            return JsonResponse(content, status=200)
+        except ValueError:
+            return JsonResponse({'message': 'INSTANCE_IS_NOT_NUMBER'}, status=400)
