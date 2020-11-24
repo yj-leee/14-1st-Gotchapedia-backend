cc4245d85de926fcd1fb98cab21b87d53ce709ee
gojulie84@gmail.com <70756464+devburgundy@users.noreply.github.com>
2020-11-25 01:36:40 +0900

    MODIFY: comment model : movie null=True

--- comment/models.py
+++ comment/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Comment(models.Model):
     user     = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    movie    = models.ForeignKey('movie.Movie', on_delete=models.CASCADE)
+    movie    = models.ForeignKey('movie.Movie', on_delete=models.CASCADE, null=True)
     comment  = models.ForeignKey(
         'Comment',
         on_delete    = models.CASCADE,
