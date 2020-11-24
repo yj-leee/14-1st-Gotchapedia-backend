cca88d6de64bbf1ce50220403d589c1e08e454fe
GyuSeok <gyskjng@gmail.com>
2020-11-24 15:47:49 +0900

    REFATOR: 선호 장르/국가 리스트 뷰 리팩토링
    - Query Set 기능만으로 카테고리별 개수,평균 및 정렬 구현

--- analysis/views.py
+++ analysis/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.views import View
 from django.http import JsonResponse
+from django.db.models import  Count, Avg
 
 from movie.models import Movie
 from analysis.models import Star
@@ -13,36 +14,24 @@ class FavoriteView(View):
         account_id = request.user
         category   = request.GET.get('category', 'genre') # '장르(genre)' 또는 '국가(country)'
 
-        results = {}
         stars = []
+        category_path = ''
 
         if category == 'genre':
             stars = Star.objects.select_related('movie').prefetch_related('movie__genre_set').filter(user_id=account_id)
-            for star in stars:
-                for genre in star.movie.genre_set.all():
-                    if not genre.name in results:
-                        results[genre.name] = {'count': 0, 'score': 0}
-                    results[genre.name]['count'] += 1
-                    results[genre.name]['score'] += star.point
+            category_path = 'movie__genre__name'
 
         if category == 'country':
             stars = Star.objects.select_related('movie').filter(user_id=account_id)
-            for star in stars:
-                if not star.movie.country in results:
-                    results[star.movie.country] = {'count': 0, 'score': 0}
-                results[star.movie.country]['count'] += 1
-                results[star.movie.country]['score'] += star.point
+            category_path = 'movie__country'
 
-        for result in results.values():
-            result['score'] = int(result['score'] / result['count'] * 20)
-
-        content = {
+        context = {
             'wholeCount': len(stars),
             'watchingTime' : sum([star.movie.show_time for star in stars]),
-            'data': sorted([{
-                        'label': key,
-                        'score': value['score'],
-                        'count': value['count'],
-            } for key, value in results.items()], key=lambda data: data['score'], reverse=True)
+            'data': [{
+                        'label': star[category_path],
+                        'score': int(star['avg']),
+                        'count': star['count'],
+                     } for star in stars.values(category_path).annotate(count=Count(category_path), avg=20 * Avg('point')).order_by('-avg')]
         }
-        return JsonResponse(content, status=200)
+        return JsonResponse(context, status=200)
