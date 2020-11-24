7243fc59c0906acc714961d2c09b34556fb1bd86
GyuSeok <gyskjng@gmail.com>
2020-11-24 13:37:01 +0900

    MODIFY: movie/urls.py 불필요한 주석제거

--- movie/urls.py
+++ movie/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views      import InterestView, InterestListView
 
 urlpatterns= [
-    #path('/movies/user', MoviesUserView.as_view()),
-    path('/interests', InterestListView.as_view()), #GET
-    path('/<int:movie_id>/interest', InterestView.as_view()),# GET/POST/FETCH/DELETE
+    path('/interests', InterestListView.as_view()),
+    path('/<int:movie_id>/interest', InterestView.as_view()),
 ]
