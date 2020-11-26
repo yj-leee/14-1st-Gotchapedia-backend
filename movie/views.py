19b82d08b1843280e989232521baa02d5d7255fa
devburgundy <gojulie84@gmail.com>
2020-11-26 17:43:48 +0900

    DELETE: ReplyListVIew 삭제

--- movie/views.py
+++ movie/views.py
@@ -362,41 +362,6 @@ class CommentLikeView(View):
         return JsonResponse (feedback, status=204)
 
 
-class ReplyListView(View):
-    @login_decorator
-    def get(self, request, comment_id):
-
-        comments = Comment.objects.select_related('user').prefetch_related('like_set').filter(comment_id=comment_id)
-
-        comment_list = [{
-            "id": comment.id,
-            "userName": comment.user.name,
-            "userImage": comment.user.profile_image,
-            "content": comment.content,
-            "likeCount": comment.like_set.count(),
-        } for comment in comments]
-
-        return JsonResponse({"data":comment_list}, status=200)
-
-    @login_decorator
-    def post(self, request, comment_id):
-        data = json.loads(request.body)
-
-        if 'content' not in data.keys():
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
-
-        if not Comment.objects.filter(id=comment_id).exists():
-            return JsonResponse({'message': 'NO_COMMENT'}, status=400)
-
-        reply = Comment.objects.create(user_id=request.user.id, comment_id=comment_id, content=data['content'])
-
-        context = {
-            'id': reply.id,
-            'content': reply.content
-        }
-        return JsonResponse(context, status=201)
-
-
 class ReplyView(View):
     @login_decorator
     def patch(self, request, reply_id):
